3469342761486fd16e751c8e2d3ae80c8e6b2762
Oryzon00 <adrian.mael.jung@gmail.com>
2024-09-05 21:09:09 +0200

    WIP scripting for P3

--- P3/router/script/conf_router.sh
+++ P3/router/script/conf_router.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
-echo "Hello world!"
+# reverse hostname, cut it in 2 by delimiter - and take first part
+HOST_NBR=$(hostname | rev | cut -d"-" -f1)
 
-exec /bin/sh
+
+if [ "${HOST_NBR}" = "1" ]; then
+	echo "launching conf_spine.sh" > /root/script/script_launched.txt
+	exec bash /root/script/conf_spine.sh
+else
+	echo "launching conf_leaf.sh" > /root/script/script_launched.txt
+	exec bash /root/script/conf_leaf.sh
+fi
